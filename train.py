61d81b67ad12de170a319fe628e15e43a703f33c
duccd <caodinhducuet@gmail.com>
2022-08-16 16:14:34 +0900

    train scannet server

--- train.py
+++ train.py
@@ -156,10 +156,10 @@ class CustomDataParallel(nn.DataParallel):
     It should also be faster than the general case.
     
     """
-    def scatter(self, inputs, kwargs, device_ids=[0]):
+    def scatter(self, inputs, kwargs, device_ids=[2]):
         # More like scatter and data prep at the same time. The point is we prep the data in such a way
         # that no scatter is necessary, and there's no need to shuffle stuff around different GPUs.
-        devices = ['cuda:{}'.format(device_ids[2])]
+        devices = ['cuda:{}'.format(device_ids[0])]
         splits = self.prepare_data(inputs[0], devices, allocation=args.batch_alloc)
 
         return [[split[device_idx] for split in splits] for device_idx in range(len(devices))], \
