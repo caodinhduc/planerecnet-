cd740a7608e88de79b3349f0b0b6656825f478e1
duccd <caodinhducuet@gmail.com>
2022-09-04 15:47:13 +0900

    train propose scannet with full data

--- data/config.py
+++ data/config.py
@@ -112,16 +112,16 @@ dataset_base = Config({
 scannet_dataset = dataset_base.copy({
     'name': 'ScanNetDataset',
 
-    'train_images': '../../../data1/duccd/used_filter_scans/',
-    'train_info':   '../../../data1/duccd/filter_scannet_train.json',
+    'train_images': '../../../data1/duccd/official/',
+    'train_info':   '../../../data1/duccd/scannet_train.json',
 
     # Validation images and annotations.
-    'valid_images': '../../../data1/duccd/used_filter_scans/',
-    'valid_info':   '../../../data1/duccd/filter_scannet_val.json',
+    'valid_images': '../../../data1/duccd/official/',
+    'valid_info':   '../../../data1/duccd/scannet_val.json',
 
     # Evaluation images and annotations.
-    # 'eval_images': '../../../data1/duccd/used_filter_scans/',
-    # 'eval_info':   '../../../data1/duccd/filter_scannet_eval.json',
+    'eval_images': '../../../data1/duccd/official/',
+    'eval_info':   '../../../data1/duccd/scannet_eval.json',
 
     # # Training images and annotations
     # 'train_images': '../scannet/filter_scans/',
@@ -132,8 +132,8 @@ scannet_dataset = dataset_base.copy({
     # 'valid_info':   'filter_scannet_val.json',
 
     # Evaluation images and annotations.
-    'eval_images': '../scannet/filter_scans/',
-    'eval_info':   'filter_scannet_eval.json',
+    # 'eval_images': '../scannet/filter_scans/',
+    # 'eval_info':   'filter_scannet_eval.json',
     
 
     # A list of names for each of you classes.
