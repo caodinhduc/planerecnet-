b3ec7c259fa1d1d8187e42e64fc2bb3b5b11f253
duccd <caodinhducuet@gmail.com>
2022-09-24 23:51:54 +0900

    attention

---
+++ attention.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class SABlock(nn.Module):
+    """ Spatial self-attention block """
+    def __init__(self, in_channels, out_channels):
+        super(SABlock, self).__init__()
+        self.attention = nn.Sequential(nn.Conv2d(in_channels, out_channels, 3, padding=1, bias=False),
+                                        nn.Sigmoid())
+        self.conv = nn.Conv2d(in_channels, out_channels, 3, padding=1, bias=False)
+
+    def forward(self, x):
+        attention_mask = self.attention(x)
+        features = self.conv(x)
+        return torch.mul(features, attention_mask)
