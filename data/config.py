774d1ddc61095736f2729295c4c1fb7ce91d5390
duccd <caodinhducuet@gmail.com>
2022-10-08 16:37:41 +0900

    add boundary loss for SA

--- data/config.py
+++ data/config.py
@@ -112,28 +112,28 @@ dataset_base = Config({
 scannet_dataset = dataset_base.copy({
     'name': 'ScanNetDataset',
 
-    # 'train_images': '../../../data1/duccd/official/',
-    # 'train_info':   '../../../data1/duccd/scannet_train.json',
-
-    # # Validation images and annotations.
-    # 'valid_images': '../../../data1/duccd/official/',
-    # 'valid_info':   '../../../data1/duccd/scannet_val.json',
-
-    # Evaluation images and annotations.
-    # 'eval_images': '../../../data1/duccd/official/',
-    # 'eval_info':   '../../../data1/duccd/scannet_eval.json',
-
-    # Training images and annotations
-    'train_images': '../scannet/official/',
-    'train_info':   'scannet_val.json',
+    'train_images': '../../../data1/duccd/official/',
+    'train_info':   '../../../data1/duccd/scannet_train.json',
 
     # Validation images and annotations.
-    'valid_images': '../scannet/official/',
-    'valid_info':   'scannet_val.json',
+    'valid_images': '../../../data1/duccd/official/',
+    'valid_info':   '../../../data1/duccd/scannet_val.json',
 
     # Evaluation images and annotations.
-    'eval_images': '../scannet/official/',
-    'eval_info':   'scannet_eval.json',
+    'eval_images': '../../../data1/duccd/official/',
+    'eval_info':   '../../../data1/duccd/scannet_eval.json',
+
+    # # Training images and annotations
+    # 'train_images': '../scannet/official/',
+    # 'train_info':   'scannet_val.json',
+
+    # # Validation images and annotations.
+    # 'valid_images': '../scannet/official/',
+    # 'valid_info':   'scannet_val.json',
+
+    # # Evaluation images and annotations.
+    # 'eval_images': '../scannet/official/',
+    # 'eval_info':   'scannet_eval.json',
     
 
     # A list of names for each of you classes.
@@ -487,7 +487,7 @@ PlaneRecNet_base_config = Config(
         'solov2': solov2_base,
 
         # Loss Settings
-        'dice_weight': 5.0,
+        'dice_weight': 10.0,
         'focal_weight': 1.0,
         'depth_weight': 5.0,
         'use_lava_loss': False,
