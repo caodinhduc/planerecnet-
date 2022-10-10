c62db184560384eee4d3c27775bf7030867ff6b0
duccd <caodinhducuet@gmail.com>
2022-10-11 00:27:03 +0900

    ok

--- data/config.py
+++ data/config.py
@@ -120,8 +120,8 @@ scannet_dataset = dataset_base.copy({
     'valid_info':   '../../../data1/duccd/scannet_val.json',
 
     # Evaluation images and annotations.
-    'eval_images': '../../../data1/duccd/official/',
-    'eval_info':   '../../../data1/duccd/scannet_eval.json',
+    # 'eval_images': '../../../data1/duccd/official/',
+    # 'eval_info':   '../../../data1/duccd/scannet_eval.json',
 
     # Training images and annotations
     # 'train_images': '../scannet/official/',
@@ -132,8 +132,8 @@ scannet_dataset = dataset_base.copy({
     # 'valid_info':   'scannet_val.json',
 
     # # # # Evaluation images and annotations.
-    # 'eval_images': '../scannet/official/',
-    # 'eval_info':   'scannet_eval.json',
+    'eval_images': '../scannet/official/',
+    'eval_info':   'scannet_eval.json',
     
 
     # A list of names for each of you classes.
