bb71c419061aff26a83c6e993a516fbe0541609b
duccd <caodinhducuet@gmail.com>
2022-10-20 14:57:29 +0900

    official training

--- models/functions/losses.py
+++ models/functions/losses.py
@@ -426,8 +426,8 @@ class BoundaryLoss(nn.Module):
         target_boundary = F.conv2d(target.unsqueeze(1), self.laplacian_kernel, padding=1).squeeze(1)
         input_boundary = F.conv2d(input.unsqueeze(1), self.laplacian_kernel, padding=1).squeeze(1)
         
-        input_boundary_2 = F.interpolate(input_boundary.unsqueeze(1), scale_factor=0.5, mode='bilinear', align_corners=False).squeeze(1)
-        target_boundary_2 = F.interpolate(target_boundary.unsqueeze(1), scale_factor=0.5, mode='bilinear', align_corners=False).squeeze(1)
+        # input_boundary_2 = F.interpolate(input_boundary.unsqueeze(1), scale_factor=0.5, mode='bilinear', align_corners=False).squeeze(1)
+        # target_boundary_2 = F.interpolate(target_boundary.unsqueeze(1), scale_factor=0.5, mode='bilinear', align_corners=False).squeeze(1)
         
         #------------------------------------------------------------------------------------------------------------
         # import os
@@ -470,13 +470,13 @@ class BoundaryLoss(nn.Module):
         #------------------------------------------------------------------------------------------------------------
         # computer for downscale 2
         
-        input2 = input_boundary_2.contiguous().view(input.size()[0], -1)
-        target2 = target_boundary_2.contiguous().view(target.size()[0], -1).float()
-        target2 = torch.abs(target2)
-        input2 = torch.abs(input2)
-        pos_index2 = (input2 >= 0.1)
-        input2 = input2[pos_index2]
-        target2 = target2[pos_index2]
+        # input2 = input_boundary_2.contiguous().view(input.size()[0], -1)
+        # target2 = target_boundary_2.contiguous().view(target.size()[0], -1).float()
+        # target2 = torch.abs(target2)
+        # input2 = torch.abs(input2)
+        # pos_index2 = (input2 >= 0.1)
+        # input2 = input2[pos_index2]
+        # target2 = target2[pos_index2]
         
         input = input_boundary.contiguous().view(input.size()[0], -1)
         target = target_boundary.contiguous().view(target.size()[0], -1).float()
@@ -486,7 +486,7 @@ class BoundaryLoss(nn.Module):
         input = input[pos_index]
         target = target[pos_index]
 
-        loss = self.loss(input, target) + 0.5 * self.loss1(input2, target2)
+        loss = self.loss(input, target)
         return loss
 
 
