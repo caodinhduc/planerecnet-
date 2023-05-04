a178026a3715cc90c7a3dd958d9409f49b8f6f62
duccd <caodinhducuet@gmail.com>
2023-05-04 23:20:25 +0900

    3

--- data/config.py
+++ data/config.py
@@ -482,7 +482,7 @@ PlaneRecNet_base_config = Config(
         'solov2': solov2_base,
 
         # Loss Settings
-        'dice_weight': 10.0,
+        'dice_weight': 3.0,
         'focal_weight': 1.0,
         'depth_weight': 5.0,
         'use_lava_loss': False,
