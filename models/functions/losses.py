62fa7ec9cb524ca9396f33040cc7e0c1fc3a327c
duccd <caodinhducuet@gmail.com>
2023-05-07 13:27:30 +0900

    train gradient with scannet

--- models/functions/losses.py
+++ models/functions/losses.py
@@ -77,6 +77,34 @@ class PlaneRecNetLoss(nn.Module):
                          for kernel_preds_level_img, grid_orders_level_img in
                          zip(kernel_preds_level, grid_orders_level)]
                         for kernel_preds_level, grid_orders_level in zip(kernel_preds, zip(*grid_order_list))]
+        
+        # Generate the gradient mask
+        valid_mask = None
+        if self.dataset_name == 'ScanNet':
+            valid_mask = torch.zeros_like(gt_depths)
+            valid_mask[:, :, 20:460, 20:620] = 1
+        if self.dataset_name == 'Stanford 2D3DS':
+            # dilate the valid mask, to filter out invalid gradient values
+            valid_mask = gt_depths>0
+            dilate_kernel = torch.autograd.Variable(torch.ones((1, 1, 5, 5)).cuda()) 
+            invalid_mask = valid_mask.logical_not().float()
+            dilate_valid_mask = F.conv2d(invalid_mask, dilate_kernel, padding=2).bool().logical_not()
+            valid_mask = dilate_valid_mask
+        #batched_gt_scale_invariant_gradients = (compute_gradient_map(depth_preds, valid_mask) / torch.pow(depth_preds.clamp(min=self.depth_resolution), 2)).detach()
+        batched_gt_scale_invariant_gradients = compute_gradient_map(gt_depths, valid_mask, mode='sum') #/ torch.pow(gt_depths.clamp(min=self.depth_resolution), 2)
+        batched_gt_scale_invariant_gradients = batched_gt_scale_invariant_gradients.clamp(max=1e-2)
+        batched_gt_scale_invariant_gradients[batched_gt_scale_invariant_gradients<1e-4] = 0
+        
+        gradient_masks = F.interpolate(batched_gt_scale_invariant_gradients, (120, 160), mode='bilinear', align_corners=False)
+        
+        
+        ins_gradient_masks = []
+        for ins_labels_level in zip(*ins_label_list):
+            ins_gradient_masks.append(torch.cat([gradient_masks[i].repeat(ins_labels_level[i].shape[0], 1, 1) for i in range(len(ins_labels_level))], 0))
+ 
+
+        
+        
         # generate masks
         ins_pred_list = []
         ins_pred_batched_list = [torch.empty(0)]*len(mask_preds) 
@@ -122,11 +150,11 @@ class PlaneRecNetLoss(nn.Module):
 
         # Boundary loss
         loss_boundary = []
-        for input, target in zip(ins_pred_list, ins_labels):
+        for input, target, gradient_mask in zip(ins_pred_list, ins_labels, ins_gradient_masks):
             if input is None:
                 continue
             input = torch.sigmoid(input)
-            loss_boundary.append(self.boundary_loss(input, target))
+            loss_boundary.append(self.boundary_loss(input, target, gradient_mask))
         loss_bdr_mean = torch.stack(loss_boundary).mean()
         losses['bdr'] = loss_bdr_mean
 
@@ -411,6 +439,30 @@ class DiceLoss(nn.Module):
         return 1 - d
 
 
+# class BoundaryLoss(nn.Module):
+#     def __init__(self):
+#         super(BoundaryLoss, self).__init__()
+#         w = 1
+#         self.laplacian_kernel = torch.zeros((2*w+1, 2*w+1), dtype=torch.float32).reshape(1,1,2*w+1,2*w+1).requires_grad_(False) - 1
+#         self.laplacian_kernel[0,0,w,w] = (2*w+1)*(2*w+1)-1
+#         self.laplacian_kernel.cuda()
+#         self.loss = nn.MSELoss().cuda()
+        
+#     def forward(self, input, target):
+#         target = target.float()
+#         target_boundary = F.conv2d(target.unsqueeze(1), self.laplacian_kernel, padding=1).squeeze(1)
+#         input_boundary = F.conv2d(input.unsqueeze(1), self.laplacian_kernel, padding=1).squeeze(1)
+#         # ------------------------------------------------------------------------------------------------------------
+      
+#         target = torch.abs(target_boundary)
+#         input = torch.abs(input_boundary)
+#         input_candidate = input > 0.2
+        
+#         input = input[input_candidate]
+#         target = target[input_candidate]
+#         loss = self.loss(input, target)
+#         return loss
+
 class BoundaryLoss(nn.Module):
     def __init__(self):
         super(BoundaryLoss, self).__init__()
@@ -420,16 +472,64 @@ class BoundaryLoss(nn.Module):
         self.laplacian_kernel.cuda()
         self.loss = nn.MSELoss().cuda()
         
-    def forward(self, input, target):
+    def forward(self, input, target, gradient_mask):
         target = target.float()
+        
+        # if self.coarse_size:
+        #     input = F.interpolate(input.unsqueeze(1), self.coarse_size, mode='bilinear', align_corners=False).squeeze(1)
+        #     target = F.interpolate(target.unsqueeze(1), self.coarse_size, mode='bilinear', align_corners=False).squeeze(1)
+        #     target = target > 0.0
+        #     target = target.float()
         target_boundary = F.conv2d(target.unsqueeze(1), self.laplacian_kernel, padding=1).squeeze(1)
         input_boundary = F.conv2d(input.unsqueeze(1), self.laplacian_kernel, padding=1).squeeze(1)
         # ------------------------------------------------------------------------------------------------------------
       
+        # import os
+        # import cv2
+        # import numpy as np
+            
+        # for i in range(target_boundary.shape[0]):
+        #     current_tensor = target_boundary[i, :, :].detach().cpu().numpy()
+        #     current_tensor = abs(current_tensor)
+        #     current_tensor = ((current_tensor - current_tensor.min()) / (current_tensor.max() - current_tensor.min()) * 255).astype(np.uint8)
+        #     tensor_color = cv2.applyColorMap(current_tensor, cv2.COLORMAP_VIRIDIS)
+        #     tensor_color_path = os.path.join('image_logs/gt_mask', '{}.png'.format(i))
+        #     cv2.imwrite(tensor_color_path, tensor_color)
+        # for i in range(target.shape[0]):
+        #     current_tensor = gradient_mask[i, :, :].detach().cpu().numpy()
+        #     current_tensor = abs(current_tensor)
+        #     current_tensor = ((current_tensor - current_tensor.min()) / (current_tensor.max() - current_tensor.min()) * 255).astype(np.uint8)
+        #     tensor_color = cv2.applyColorMap(current_tensor, cv2.COLORMAP_VIRIDIS)
+        #     tensor_color_path = os.path.join('image_logs/gradient', '{}.png'.format(i))
+        #     cv2.imwrite(tensor_color_path, tensor_color)
+        # print('')
+        mask_weight = torch.ones_like(target_boundary)
+        index = list(torch.where(abs(target_boundary[:, 1: self.max_size[0] - 1, 1: self.max_size[1] -1]) > 0.2))
+        index[1] += 1
+        index[2] += 1
+        weight = torch.std(torch.stack([gradient_mask[index[0], index[1], index[2]], gradient_mask[index[0], index[1], index[2] - 1], gradient_mask[index[0], index[1], index[2] + 1],
+                              gradient_mask[index[0], index[1] - 1, index[2]], gradient_mask[index[0], index[1] + 1, index[2]], 
+                              gradient_mask[index[0], index[1] + 1, index[2] + 1], gradient_mask[index[0], index[1] + 1, index[2] -1],
+                              gradient_mask[index[0], index[1] - 1, index[2] + 1], gradient_mask[index[0], index[1] - 1, index[2] -1]], -1), dim=-1)
+        # mean = torch.mean(weight)
+        # s = torch.sum(mask_weight[index[0], index[1], index[2]])
+        weight = (weight - weight.min() ) / ( weight.max() - weight.min())
+        
+        mask_weight[index[0], index[1], index[2]] = 3 * weight
+        mask_weight[index[0], index[1], index[2]] += 0.5
+        # input = input_boundary.contiguous().view(input.size()[0], -1)
+        # target = target_boundary.contiguous().view(target.size()[0], -1)
+        # print(torch.sum(weight))
         target = torch.abs(target_boundary)
         input = torch.abs(input_boundary)
         input_candidate = input > 0.2
-        
+        # target_candidate = target > 0.2
+        # candidate = input_candidate + target_candidate
+        # s1 = torch.sum(mask_weight[index[0], index[1], index[2]])
+        # s2 = torch.sum(target_candidate)
+        # s3 = torch.sum(candidate)
+        input *= mask_weight
+        target *= mask_weight
         input = input[input_candidate]
         target = target[input_candidate]
         loss = self.loss(input, target)
